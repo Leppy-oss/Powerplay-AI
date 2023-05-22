31c88bf8b1b02ef67bee3ae08cbecb47dd8bf26a
Leppy-oss <70586381+Leppy-oss@users.noreply.github.com>
2023-05-22 16:51:28 -0500

    working on improving the game engine in pygame. apparently wasabi2d has outdated pygame dependencies and hasnt been uipdated since 2020 so it wont work

--- game/src/main/game_object.py
+++ game/src/main/game_object.py
@@ -3,6 +3,7 @@ import pygame
 from sprite import Sprite
 from utils import constants
 
+
 class GameObject(Sprite):
     def __init__(self, surface: pygame.Surface, scale: float = 1, opt_url: str = None, w: float = None, h: float = None, bb_l: float = None, bb_t: float = None, bb_h: float = None, bb_w: float = None, x: float = 0, y: float = 0) -> None:
         surface_to_use: pygame.Surface = None
@@ -10,7 +11,7 @@ class GameObject(Sprite):
             surface_to_use = pygame.image.load(constants.RES_URL + opt_url)
         else:
             surface_to_use = surface
-            
+
         super().__init__(surface_to_use, scale, w, h, x, y)
         if bb_l is not None:
             self.bb_l = bb_l
@@ -28,15 +29,19 @@ class GameObject(Sprite):
             self.bb_h = bb_h
         else:
             self.bb_h = self.surface.get_bounding_rect().height
-            
-        self.bb: pygame.Rect = pygame.Rect(self.bb_l, self.bb_t, self.bb_w, self.bb_h)
-        
+
+        self.bb: pygame.Rect = pygame.Rect(
+            self.bb_l, self.bb_t, self.bb_w, self.bb_h)
+
     def collide(self, other: pygame.Rect) -> bool:
         pass
-    
+
     def update(self, dt: float) -> None:
-        pass
-    
+        self.bb_l = self.x - (self.bb_w - self.surface.get_width()) / 2
+        self.bb_t = self.y - (self.bb_h - self.surface.get_height()) / 2
+        self.bb = pygame.Rect(self.bb_l, self.bb_t, self.bb_w, self.bb_h)
+
     def render(self, display: pygame.Surface) -> None:
         display.blit(self.surface, (self.x, self.y))
-        pygame.draw.rect(display, (0, 255, 0), self.bb, width=2)
+        if constants.DEBUG:
+            pygame.draw.rect(display, (0, 255, 0), self.bb, width=2)
--- game/src/main/robot.py
+++ game/src/main/robot.py
@@ -1,55 +1,31 @@
 import pygame
 from utils import constants
+from game_object import GameObject
 
-class Robot:
-    def __init__(self, scale, sprite_url, x=0, y=0, w=0, h=0, dx=0, dy=0, cones=0, bb_x=-1e9, bb_y=-1e9, bb_w=0, bb_h=0) -> None:
-        self.w = w
-        self.h = h
-        self.x = x
-        self.y = y
+
+class Robot(GameObject):
+    def __init__(self, scale: float, sprite_url: str, w: float = None, h: float = None, x: float = 0, y: float = 0, bb_l: float = None, bb_t: float = None, bb_w: float = None, bb_h: float = None, dx: float = 0, dy: float = 0, cones: float = 0):
+        super().__init__(None, scale, sprite_url, w, h, bb_l, bb_t, bb_h, bb_w, x, y)
         self.dx = dx
         self.dy = dy
-        sprite_surface = pygame.image.load(constants.RES_URL + sprite_url)
-        if (w < 1e-8):
-            w = sprite_surface.get_width() * scale
-            
-        if (h < 1e-8):
-            h = sprite_surface.get_height() * scale
-            
-        self.w = w
-        self.h = h
-        self.sprite = pygame.transform.scale(sprite_surface, (self.w, self.h))
-        self.cones = cones
-        
-        if(bb_w < 1e-8):
-            bb_w = constants.DEFAULT_BB_OFFSET_X_SCALE * 2 * self.w
-        
-        if(bb_h < 1e-8):
-            bb_h = (1 + constants.DEFAULT_BB_OFFSET_Y_SCALE * 2) * self.w
-        
-        if (bb_x < -1e8):
-            bb_x = self.x - bb_w * 0.5
-            
-        if (bb_y < -1e8):
-            bb_y = self.y - bb_y * 0.5
-            
-        self.bb = pygame.Rect(bb_x, bb_y, bb_w, bb_h)
-        
+        self.cones: float = cones
+
     def grab_cone(self):
         self.cones
-        
+
     def collideRect(self, other: pygame.Rect):
         return self.bb.colliderect(other)
-        
+
     def getTrueX(self):
-        return self.x + 0.5 * self.w
-    
+        return self.bb.centerx
+
     def getTrueY(self):
-        return self.y + 0.5 * self.h
-        
+        return self.bb.centery
+
     def update(self, dt):
         self.x += self.dx * dt
         self.y += self.dy * dt
-        
+        super().update(dt)
+
     def render(self, display: pygame.Surface):
-        display.blit(self.sprite, (self.x, self.y))
+        super().render(display)
